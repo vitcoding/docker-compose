f6446b70db05b10815ce84df52dd8ac1d7646f16
Vitaly_D <vitcode.mail@gmail.com>
2024-10-12 17:07:09 +0300

    Workable static 01

--- app/example/settings.py
+++ app/example/settings.py
@@ -127,14 +127,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-if sys.argv[1] == "runserver":
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
